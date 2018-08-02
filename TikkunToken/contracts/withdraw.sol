b7fc56fefd916c3ca45aece9ec8ad915cf6cae39
DeNgoash <40598521+DeNgoash@users.noreply.github.com>
2018-08-02 21:46:02 +0200

    Update withdraw.sol

--- TikkunToken/contracts/withdraw.sol
+++ TikkunToken/contracts/withdraw.sol
@@ -7,16 +7,18 @@ contract WithDrawal is TikkunToken {
     uint private dailyWithdraw = 5000;
     uint private spentToday;
     uint private presentDay = today();
-    // This function determines today's index/ midnight.
+    
+    // This function determines today's index at midnight.
     function today() private constant returns (uint) { return now - (now % 1 days); } 
+    
     // This function is used to withdraw 
     function withDrawals ( address _to, uint _tokens) public {
         require(isUnderLimit(_tokens), "You have reached your daily withdrawal limit");
         balances[msg.sender] -= _tokens;
         balances[_to] += _tokens;
         spentToday += _tokens;
-        //emit Transfer(msg.sender, _to, _tokens);
     }
+    
     // This function check if there is still enough tokens to withdraw within in that day
     // it also reset the amount tokens already withdrawn/spent if its a different day
     function isUnderLimit( uint _tokens) internal returns (bool) {
